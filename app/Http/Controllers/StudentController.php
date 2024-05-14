d524d8bc64fefb66a1bc2ff7670915c2036523db
Stephen Meycelino Fransisco <stephenmeycelinowork88@gmail.com>
2024-05-14 22:31:32 +0700

    🎉 add: Structure Folder with Display students data

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\StudentResource;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = StudentResource::collection(Student::paginate(10));
+
+        return inertia('Student/Index', [
+            'students' => $students,
+        ]);
+    }
+}
